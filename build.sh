19f36a73a6b018632139e62c9ffac0739c660f4c
Yuri Niyazov <yuri.niyazov@gmail.com>
2015-04-07 15:25:53 -0700

    Don't show in the dock and in the switching bar

--- build.sh
+++ build.sh
@@ -2,3 +2,4 @@
 mkdir -p build
 raco exe -o build/engine src/engine.rkt
 raco exe -o build/bridge --gui src/bridge.rkt
+/usr/libexec/PlistBuddy build/bridge.app/Contents/Info.plist -c 'Add :LSUIElement string 1'
